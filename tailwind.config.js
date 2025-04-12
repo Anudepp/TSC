08369dce580257567e8cdcbf09dda4efac3366c1
Anudeep Debbata <30561588+Anudepp@users.noreply.github.com>
2025-04-12 21:46:49 +0530

    feature/navbar-animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        slideDown: "slideDown 0.4s ease-out"
+      },
+      keyframes: {
+        slideDown: {
+          "0%": { transform: "translateY(-20%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
